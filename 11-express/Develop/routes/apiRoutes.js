28b72c8652cda79c1deb757dacc6d1458da2ecdf
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-02-26 15:54:11 -0600

    Fixed File Path For notes.html

--- 11-express/Develop/routes/apiRoutes.js
+++ 11-express/Develop/routes/apiRoutes.js
@@ -1,7 +1,32 @@
 var noteData = require("../db/db.json");
+const fs = require('fs');
 
 module.exports = function (app) {
-    app.get("/api/stores", function (req, res) {
-        res.json(noteData)
+
+    //gets data in note array to post
+    app.get("/api/notes", function (req, res) {
+        let savedNotes = [];
+
+        fs.readFile(__dirname + noteData, 'utf8', (err,data) => {
+            if(err) {
+                console.error(err);
+                res.json({"status": 404, "message": "db.json not found"});
+            } else {
+                savedNotes = JSON.parse(data);
+            }
+            res.join(savedNotes);
+        })
+    });
+
+    //posts new note data to note array
+    app.post("api/stores", function (req, res) {
+
+        
+    })
+
+    //deletes note
+    app.delete("api/stores", function (req, res) {
+
+        
     })
 }
