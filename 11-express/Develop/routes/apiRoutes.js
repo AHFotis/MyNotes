57458e79871a77a55f727bf094faad6bb87285b1
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-02-26 16:14:56 -0600

    Fixed File Path For Public Folder

--- 11-express/Develop/routes/apiRoutes.js
+++ 11-express/Develop/routes/apiRoutes.js
@@ -1,22 +1,24 @@
+
+//maybe try this on the server page to start/make sure its working, THEN move to alternate files.
 var noteData = require("../db/db.json");
 const fs = require('fs');
 
 module.exports = function (app) {
 
-    //gets data in note array to post
-    app.get("/api/notes", function (req, res) {
-        let savedNotes = [];
+    //copied from web for reference
+    // app.get("/api/notes", function (req, res) {
+    //     let savedNotes = [];
 
-        fs.readFile(__dirname + noteData, 'utf8', (err,data) => {
-            if(err) {
-                console.error(err);
-                res.json({"status": 404, "message": "db.json not found"});
-            } else {
-                savedNotes = JSON.parse(data);
-            }
-            res.join(savedNotes);
-        })
-    });
+    //     fs.readFile(__dirname + noteData, 'utf8', (err,data) => {
+    //         if(err) {
+    //             console.error(err);
+    //             res.json({"status": 404, "message": "db.json not found"});
+    //         } else {
+    //             savedNotes = JSON.parse(data);
+    //         }
+    //         res.join(savedNotes);
+    //     })
+    // });
 
     //posts new note data to note array
     app.post("api/stores", function (req, res) {
--- 11-express/server.js
+++ 11-express/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const app = express();
 
 const PORT = process.env.PORT || 8000;
@@ -6,10 +7,11 @@ const PORT = process.env.PORT || 8000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static('public'));
+app.use(express.static('Develop/public'));
 
 //Getting routes on server
 require("./Develop/routes/apiRoutes")(app);
 require("./Develop/routes/htmlRoutes")(app);
+
 
 app.listen(PORT, () => console.log(`App listening on PORT: ${PORT}`))
