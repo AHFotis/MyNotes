026d2968ce2ee7f21710be006ca4e6c9752591a9
Anna Fotis <73958785+AnHiFo@users.noreply.github.com>
2021-03-01 17:53:33 -0600

    Delete Method Succsessful

--- 11-express/Develop/routes/apiRoutes.js
+++ 11-express/Develop/routes/apiRoutes.js
@@ -13,7 +13,6 @@ module.exports = function (app) {
     app.get("/api/notes", function (req, res) {
         fs.readFile(dbPath, 'utf8', (err, data) => {
             if (err) throw err;
-            console.log(data);
             var notesResponse = JSON.parse(data)
             res.json(notesResponse);
         })
@@ -24,17 +23,23 @@ module.exports = function (app) {
         //most likely fs.appendfile
         fs.readFile(dbPath, 'utf8', (err, data) => {
             if (err) throw err;
+
             let savedNotes;
+
             if (data) {
                 savedNotes = JSON.parse(data)
             }
+
             let newNote = req.body
+
             newNote.id = uuidv4()
+
             if (savedNotes) {
                 savedNotes.push(newNote);
             } else {
                 savedNotes = [newNote];
             }
+
             fs.writeFile(dbPath, JSON.stringify(savedNotes), (err, data) => {
                 if (err) throw err;
                 res.json(newNote);
@@ -44,8 +49,28 @@ module.exports = function (app) {
     })
 
     //deletes note
-    app.delete("/api/notes", function (req, res) {
-        //maybe read, splice, and rewrite?
+    app.delete("/api/notes/:id", function (req, res) {
+
+        let deleteId = req.params.id
+
+        fs.readFile(dbPath, 'utf8', (err, data) => {
+            if (err) throw err;
+
+            let noteArray = JSON.parse(data);
+            console.log(noteArray)
+
+            for (let i = 0; i < noteArray.length; i++) {
+                if (deleteId == noteArray[i].id) {
+                    noteArray.splice(i, 1)
+                }
+            }
+
+            fs.writeFile(dbPath, JSON.stringify(noteArray), (err, data) => {
+                if (err) throw err;
+                res.send();
+               
+            })
+        })
 
     })
 }
